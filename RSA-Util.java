1d51431a0a434a3e8861bae4832443c2499377b0
Mohamed El Ghazali <48661966+mohamedelghazali@users.noreply.github.com>
2020-10-26 03:59:09 +0100

    Create RSA-Util.java

---
+++ RSA-Util.java
@@ -0,0 +1,14 @@
+public static PublicKey getPublicKey(String base64PublicKey){
+        PublicKey publicKey = null;
+        try{
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(base64PublicKey.getBytes()));
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            publicKey = keyFactory.generatePublic(keySpec);
+            return publicKey;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return publicKey;
+    }
